b9fe48652d4935a568361982e907103e5fe041d2
aleksandrastol8@gmail.com <aleksandrastol8@gmail.com>
2023-11-04 19:12:10 +0500

    Task2

---
+++ Tyuiu.ShtolAA.Sprint4.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShtolAA.Sprint4.Task2.V10.Lib;
+
+namespace Tyuiu.ShtolAA.Sprint4.Task2.V10.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] array = {4,5,6,4,1,2,3,3,2,1,3,4,5};
+            int res = ds.Calculate(array);
+            int wait = 675;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShtolAA.Sprint4.Task2.V10/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ShtolAA.Sprint4.Task2.V10.Lib;
+
+
+namespace Tyuiu.ShtolAA.Sprint4.Task2.V10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            DataService ds = new DataService();
+            Console.Title = "Спринт #4 | Выполнила: Штоль А.А. | ИИПБ-23-3";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #10                                                             *");
+            Console.WriteLine("* Выполнила: Штоль Александра Алексеевна | ИИПБ-23-3                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 13 элементов заполненный         *");
+            Console.WriteLine("* случайными в диапазоне от 1 до 6 подсчитать произведение нечетных       *");
+            Console.WriteLine("* элементов массива.                                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            
+            Console.WriteLine("Введите количество элементов массива:");
+            int len = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[len];
+
+
+            for (int i = 0; i < len; i++)
+            {
+                array[i] = rnd.Next(1, 7);
+            }
+       
+            Console.WriteLine("Получившийся массив:");
+
+            for (int i = 0; i < len; i++)
+            {
+                Console.WriteLine(array[i] + "\t");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+          
+            Console.WriteLine(" Произведение нечётных элементов массива = " + ds.Calculate(array));
+            Console.ReadKey();
+        }
+    }
+}
