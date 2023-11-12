96f5adc9a0e69e548f1a250ec7c08eefbcf1d716
aleksandrastol8@gmail.com <aleksandrastol8@gmail.com>
2023-11-12 18:01:55 +0500

    Task4

---
+++ Tyuiu.ShtolAA.Sprint4.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ShtolAA.Sprint4.Task4.V30.Lib;
+
+namespace Tyuiu.ShtolAA.Sprint4.Task4.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+
+            int[,] array = { {3, 4, 6, 6, 5},
+                             { 6, 7, 6, 3, 5},
+                             { 5, 3, 5, 7, 6},
+                             { 6, 4, 7, 6, 6 },
+                             { 7, 4, 3, 4, 5}};
+           
+            int[,] wait = {{0, 4, 6, 6, 0},
+                        { 6, 0, 6, 0, 0},
+                        { 0, 0, 0, 0, 6},
+                        { 6, 4, 0, 6, 6 },
+                        { 0, 4, 0, 4, 0}}; 
+
+            int[,] res = ds.Calculate(array);
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ShtolAA.Sprint4.Task4.V30.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ShtolAA.Sprint4.Task4.V30.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ShtolAA.Sprint4.Task4.V30.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0879d429-3fc5-4166-a999-ad792dc6a07d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
